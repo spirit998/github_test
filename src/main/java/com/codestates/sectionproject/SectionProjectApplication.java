5fecd08add6cbb7e36df38fa37cb47e583484660
spirit998 <spirit998@naver.com>
2023-08-09 03:40:37 +0900

    initial

---
+++ src/main/java/com/codestates/sectionproject/SectionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.sectionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectionProjectApplication.class, args);
+	}
+
+}
